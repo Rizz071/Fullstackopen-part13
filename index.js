81b14a1698ce609c799a346b2745022799c2ce23
Igor071 <iburykin071@gmail.com>
2024-02-02 23:43:28 +0200

    Exercise 13.3.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const main = async () => {
 
         const blogs = await sequelize.query("SELECT author,url,title, likes FROM blogs", { type: QueryTypes.SELECT })
         console.log(blogs.map(blog => blog.author + ": \'" + blog.title + "\', " + blog.likes + " likes").join("\n"))
+
         sequelize.close()
     } catch (error) {
         console.error('Unable to connect to the database:', error)
