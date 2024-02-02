d926f0130b0e6be58031b13e9f81aefca20cd3bf
Igor071 <iburykin071@gmail.com>
2024-02-02 23:29:30 +0200

    Exercise 13.3.

---
+++ commands.sql
@@ -0,0 +1,2 @@
+insert into blogs (author, url, title) values ('First author', 'http://123.org', 'First blog title');
+insert into blogs (author, url, title) values ('Second author', 'http://567.org', 'Second blog title');
