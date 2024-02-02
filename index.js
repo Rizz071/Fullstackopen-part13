d25a5aade3dd159db71f0d7a21bedd9e8edec753
Igor071 <iburykin071@gmail.com>
2024-02-02 23:43:15 +0200

    Exercise 13.3.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const { Sequelize, QueryTypes } = require('sequelize')
-const express = require('express')
-const app = express()
+
 
 const sequelize = new Sequelize(process.env.DATABASE_URL)
 
@@ -18,13 +17,3 @@ const main = async () => {
 }
 
 main()
-
-// app.get('/api/blogs', async (req, res) => {
-//     const notes = await sequelize.query("SELECT * FROM notes", { type: QueryTypes.SELECT })
-//     res.json(notes)
-// })
-
-// const PORT = process.env.PORT || 3001
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`)
-// })
